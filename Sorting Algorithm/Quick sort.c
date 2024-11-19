f3f1f1d64adf006419328bd7ab5fde26802682a8
Rohit R M <163559461+Rohit-R-M@users.noreply.github.com>
2024-11-19 19:04:47 +0530

    Quick sort.c

--- Sorting Algorithm/Quick sort.c
+++ Sorting Algorithm/Quick sort.c
@@ -1,71 +1,76 @@
-#include <stdio.h>
-#include <stdlib.h>
+//Quick Sort
 
-void swap(int* a, int* b) 
+#include<stdio.h>
+#include<stdlib.h>
+#include<time.h>
+
+int partition(int arr[],int low,int high)
 {
-    int t = *a;
-    *a = *b;
-    *b = t;
-}
-
-int partition(int arr[], int low, int high) 
-{
-
-    int pivot = arr[high];
-    int i = low - 1;
-
-    for (int j = low; j <= high - 1; j++) 
+	int key = arr[low];
+	int start = low;
+	int end = high;
+	int temp;
+	
+	while(start<end)
 	{
-        if (arr[j] < pivot) 
+		while(arr[start] <= key && start<high)
+			start++;
+	
+		while(arr[end]>key)
+			end--;
+			
+		if(start<end)     //swap start and end if the elements are in wrong place
 		{
-            i++;
-            swap(&arr[i], &arr[j]);
-        }
-    }
-    swap(&arr[i + 1], &arr[high]);
-    return i + 1;
+			temp = arr[start];
+			arr[start]=arr[end];
+			arr[end]=temp;
+		}
+	}
+	temp=arr[low];
+	arr[low]=arr[end];
+	arr[end]=temp;
+	
+	return end;
 }
 
-void quickSort(int arr[], int low, int high) 
+int quicksort(int arr[],int low,int high)
 {
-  
-    if (low < high) 
+	if(low<high)
 	{
-      
-        int pi = partition(arr, low, high);
-
-        quickSort(arr, low, pi - 1);
-        quickSort(arr, pi + 1, high);
-    }
-}
-
-void printArray(int arr[], int size) 
-{
-    for (int i = 0; i < size; i++) 
-	{
-        printf("%d ", arr[i]);
-    }
-    printf("\n");
+		int loc = partition(arr,low,high);
+		quicksort(arr,low,loc-1);
+		quicksort(arr,loc+1,high);
+	}
 }
 
 int main()
 {
-	int i,j,n;
+	int n,i;
 	
-    printf("Enter the size of the array: \n");
+	printf("Enter the size of the array:\n");
 	scanf("%d",&n);
 	
 	int arr[n];
 	
-	printf("Enter the %d numbers: \n",n);
-	for (i=0;i<n;i++)
-		scanf("%d",&arr[i]);
-		
-    n = sizeof(arr) / sizeof(arr[0]);
-
-    quickSort(arr, 0, n - 1);
-
-    printf("Sorted Array: \n");
-    printArray(arr, n);
-    return 0;
+	printf("Generating %d elements:\n",n);
+	for(i=0;i<n;i++)
+	{
+		arr[i]=rand()%1000;
+		printf("%d ",arr[i]);
+	}
+	
+	float start =clock();
+	
+	quicksort(arr,0,n-1);
+	
+	float end =clock();
+	
+	float tt=end-start/CLOCKS_PER_SEC;
+	
+	printf("\nSorted Elements are:\n");
+	for(i=0;i<n;i++)
+		printf("%d ",arr[i]);
+	printf("\n");
+	
+	printf("Total Time Taken %f",tt);
 }
